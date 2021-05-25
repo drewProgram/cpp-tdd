cdd0c786adb4131ebafd611fb8eea178cea2aea6
Andrew Filgueiras <andrew.lascasas@gmail.com>
2021-05-25 20:06:18 -0300

    added comment to test-main

--- Auction/tests/main-test.cpp
+++ Auction/tests/main-test.cpp
@@ -1,4 +1,6 @@
 // catch2 macro
+// all the framework code is incorporated with this macro, so it need to be in a isoleted file
+// in order to compilate once.
 #define CATCH_CONFIG_MAIN
 
 #include "Catch.h"
